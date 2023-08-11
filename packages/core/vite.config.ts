f200294de94ee673ef608d24b2d89798d4ddb083
litianyi.troy <litianyi.troy@bytedance.com>
2023-08-11 23:29:34 +0800

    chore: update build targets

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,44 +1,42 @@
-import { defineConfig } from 'vite'
-import * as path from 'path'
-import react from '@vitejs/plugin-react'
-import pages from 'vite-plugin-react-pages'
-import dts from 'vite-plugin-dts'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
-import * as packageJson from './package.json'
+import { defineConfig } from "vite";
+import * as path from "path";
+import dts from "vite-plugin-dts";
+import tsConfigPaths from "vite-tsconfig-paths";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import * as packageJson from "./package.json";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
 
 const libConfig = defineConfig({
   plugins: [
     dts({
-      outputDir: 'dist',
+      outputDir: "dist",
     }),
     tsConfigPaths(),
     cssInjectedByJsPlugin({
       jsAssetsFilterFunction: (chunk) => {
-        return chunk.fileName === 'index.js'
+        return (
+          chunk.fileName === "index.es.js" || chunk.fileName === "index.cjs.js"
+        );
       },
+    }),
+    getBabelOutputPlugin({
+      presets: ["@babel/preset-env"],
     }),
   ],
   build: {
-    minify: false,
+    minify: true,
     lib: {
       entry: {
-        index: path.resolve('src', 'index.tsx'),
-        // ...animations.reduce<Record<string, string>>((acc, animation) => {
-        //   const dirname = path.dirname(animation)
-        //   const basename = path.basename(animation, path.extname(animation))
-        //   acc[path.join(dirname, basename)] = path.join('src', animation)
-        //   return acc
-        // }, {}),
+        index: path.resolve("src", "index.tsx"),
       },
       name: packageJson.name,
-      formats: ['es'],
-      fileName: (_, entryName) => `${entryName}.js`,
+      formats: ["es", "cjs"],
+      fileName: (format, entryName) => `${entryName}.${format}.js`,
     },
     rollupOptions: {
       external: [...Object.keys(packageJson.peerDependencies)],
     },
   },
-})
+});
 
-export default libConfig
+export default libConfig;
